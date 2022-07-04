c0624f9ade70cb355b7d1360505a128927ebebb1
Rahul Negi <hrahulnegi@gmail.com>
2022-07-05 01:35:31 +0530

    Added functionality for hover,and clear button

--- index.js
+++ index.js
@@ -1,21 +1,54 @@
-console.log("in js")
-let size = 10
-const grid = document.querySelector('.grid');
-grid.style.gridTemplateColumns = `repeat(${size},1fr)`;
-grid.style.gridTemplateColumns = `repeat(${size},1fr)`;
-let n =5;
-let i = 0;
+createGrid(50);
 
+function createGrid(size)
+{
+    const grid = document.querySelector('.grid');
+     // logic for removing pre-exisiting divs 
+    let squares = grid.querySelectorAll('div');
+    squares.forEach(div => div.remove());
+    // fr stands for fraction of size
+    grid.style.gridTemplateColumns = `repeat(${size},1fr)`;
+    grid.style.gridTemplateColumns = `repeat(${size},1fr)`;
+
+    let i = 0;
+    while(i!==(size*size))
+    {
+        grid.appendChild(createDiv());
+        console.log("div created");
+        i++;
+    }
+}
+
+
+//method for creating divs which we need to append to the grid cells
 function createDiv(){
     const div = document.createElement('div');
-    div.style.backgroundColor = 'yellow';
+    //added event listener to every div created inside the grid
+    div.addEventListener("mouseover",e=>{
+        e.target.style.backgroundColor = setColor();
+    });
+    div.style.backgroundColor = 'white';
     return div;
 }
 
-
-while(i!==(size*size))
+//method for fetching color value set in Color Picker
+function setColor()
 {
-    grid.appendChild(createDiv());
-    console.log("div created")
-    i++;
+    return document.querySelector('.colorPicker').value
 }
+
+
+//method is triggered by slider 
+function changeSize(size)
+{
+    let rangeValue = document.querySelector(".rangeVal");
+    createGrid(size);
+    rangeValue.innerHTML = `${size} * ${size} `;
+}
+
+let clearBtn = document.querySelector('.clearBtn');
+clearBtn.addEventListener("click",e=>{
+    let slider = document.querySelector(".slider");
+    createGrid(slider.value);
+    console.log(slider.value);
+})
